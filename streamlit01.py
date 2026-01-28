0f6b48deefe1b84d596c11ad7c475fa354577a55
Pantoffel24 <christischoeman@gmail.com>
2026-01-28 10:47:42 +0200

    made some text changes

--- streamlit01.py
+++ streamlit01.py
@@ -7,5 +7,7 @@ This is going to be a large block of text for testing purposes.
 
 Do paragraphs work? Maybe. $\int x^2 dt$ here is an integral. Fascinating.
 **bold text**
+
+This is pretty cool. I can share my information online yippie!
 """
 st.write(intro_text)
